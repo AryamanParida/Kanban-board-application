16a867d7fec7c270e3540ba1a6cb595e0a12ec76
Aryaman Parida <77320949+AryamanParida@users.noreply.github.com>
2023-12-10 08:05:53 +0530

    Add files via upload

---
+++ src/Components/Tags/Tag.js
@@ -0,0 +1,38 @@
+<<<<<<< HEAD
+import React from 'react'
+import "./Tag.css";
+import { Ellipse } from 'react-ionicons'
+
+function Tag(props) {
+  return (
+    <div className='tag'>
+        <Ellipse
+        color={'#00000'}
+        height="10px"
+        width="10px"
+      />
+        {props.text}
+    </div>
+  )
+}
+
+=======
+import React from 'react'
+import "./Tag.css";
+import { Ellipse } from 'react-ionicons'
+
+function Tag(props) {
+  return (
+    <div className='tag'>
+        <Ellipse
+        color={'#00000'}
+        height="10px"
+        width="10px"
+      />
+        {props.text}
+    </div>
+  )
+}
+
+>>>>>>> 72b503cdd826ba110024b816ad86b2d978906f18
+export default Tag
